b4dc753ac561bd6f8ada09ff6ee3091d842b1ea2
Hona <nguyenmanhhoan2k4@gmail.com>
2025-08-03 22:37:20 +0700

    create navbar and routers

---
+++ src/components/Login/Login.js
@@ -0,0 +1,12 @@
+import { Button } from "bootstrap";
+
+const Login = (props) => {
+    return (
+        <div className="login-container">
+            Login
+            <button>test bootstrap</button>
+        </div>
+    );
+}
+
+export default Login;
