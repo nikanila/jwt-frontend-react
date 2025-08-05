92d9a508125036d4d5e3d4ef7ed7f7f5afa15abc
Hona <nguyenmanhhoan2k4@gmail.com>
2025-08-05 22:11:56 +0700

    create form login responsive

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,12 +1,43 @@
-import { Button } from "bootstrap";
+import "./Login.scss";
 
 const Login = (props) => {
-    return (
-        <div className="login-container">
-            Login
-            <button>test bootstrap</button>
+  return (
+    <div className="login-container">
+      <div className="container">
+        <div className="row px-3 px-sm-0">
+          <div className="content-left col-12 d-none col-sm-7 d-sm-block">
+            <div className="brand">Hoan's website</div>
+            <div className="detail">
+              Learning reactjs to build a great UI for the website
+            </div>
+          </div>
+          <div className="content-right col-sm-5 col-12 d-flex flex-column gap-3 py-3">
+            <div className="brand d-sm-none ">Hoan's website</div>
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Email address or phone number"
+            ></input>
+            <input
+              type="password"
+              className="form-control"
+              placeholder="password"
+            ></input>
+            <button className="btn btn-primary">Login</button>
+            <span className="text-center">
+              <a className="forgot-password" href="#">
+                Forgot your password?
+              </a>
+            </span>
+            <hr />
+            <div className="text-center">
+              <button className="btn btn-success">Create new account</button>
+            </div>
+          </div>
         </div>
-    );
-}
+      </div>
+    </div>
+  );
+};
 
 export default Login;
