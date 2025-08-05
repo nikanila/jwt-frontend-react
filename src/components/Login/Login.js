cf7a9bd84cccee7d69c5c7836ae544da3258771f
Hona <nguyenmanhhoan2k4@gmail.com>
2025-08-05 22:37:50 +0700

    create form register responsive

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,6 +1,12 @@
+import userEvent from "@testing-library/user-event";
 import "./Login.scss";
+import { useHistory } from "react-router-dom";
 
 const Login = (props) => {
+    let history = useHistory();
+  const handleCreateNewAccount = () => {
+    history.push('/register');
+  };
   return (
     <div className="login-container">
       <div className="container">
@@ -31,7 +37,12 @@ const Login = (props) => {
             </span>
             <hr />
             <div className="text-center">
-              <button className="btn btn-success">Create new account</button>
+              <button
+                className="btn btn-success"
+                onClick={() => handleCreateNewAccount()}
+              >
+                Create new account
+              </button>
             </div>
           </div>
         </div>
---
+++ src/components/Register/Register.js
@@ -0,0 +1,80 @@
+import userEvent from "@testing-library/user-event";
+import "./Register.scss";
+import { useHistory } from "react-router-dom";
+
+const Register = (props) => {
+  let history = useHistory();
+  const handleLogin = () => {
+    history.push("/login");
+  };
+  return (
+    <div className="register-container">
+      <div className="container">
+        <div className="row px-3 px-sm-0">
+          <div className="content-left col-12 d-none col-sm-7 d-sm-block">
+            <div className="brand">Hoan's website</div>
+            <div className="detail">
+              Learning reactjs to build a great UI for the website
+            </div>
+          </div>
+          <div className="content-right col-sm-5 col-12 d-flex flex-column gap-3 py-3">
+            <div className="brand d-sm-none ">Hoan's website</div>
+            <div className="form-group">
+              <label>Email:</label>
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Email address"
+              ></input>
+            </div>
+            <div className="form-group">
+              <label>Phone number:</label>
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Phone number"
+              ></input>
+            </div>
+             <div className="form-group">
+              <label>Username:</label>
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Username"
+              ></input>
+            </div>
+            <div className="form-group">
+              <label>Password:</label>
+              <input
+                type="password"
+                className="form-control"
+                placeholder="password"
+              ></input>
+            </div>
+            <div className="form-group">
+              <label>Re-enter password:</label>
+              <input
+                type="password"
+                className="form-control"
+                placeholder="Re-enter password"
+              ></input>
+            </div>
+            <button className="btn btn-primary">Register</button>
+            
+            <hr />
+            <div className="text-center">
+              <button
+                className="btn btn-success"
+                onClick={() => handleLogin()}
+              >
+                Already've an account. Login
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Register;
