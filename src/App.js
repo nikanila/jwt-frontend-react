785064d3eda73c3d6835a177c2ed819e440c1b09
Hona <nguyenmanhhoan2k4@gmail.com>
2025-08-13 22:23:15 +0700

    add Users component and use sessionStorage to manage login session

--- src/App.js
+++ src/App.js
@@ -5,12 +5,22 @@ import Login from "./components/Login/Login";
 import Register from "./components/Register/Register";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import Users from "./components/ManageUsers/Users";
+import { useEffect, useState } from "react";
+import _ from "lodash";
 
 function App() {
+  const [account, setAccount] = useState({});
+  useEffect(() => {
+    let session = sessionStorage.getItem("account");
+    if (session) {
+      setAccount(JSON.parse(session));
+    }
+  }, []);
   return (
     <Router>
       <div className="app-container">
-        {/* <Nav /> */}
+        {account && !_.isEmpty(account) && account.isAuthenticated && <Nav /> }
         <Switch>
           <Route path="/news">news</Route>
           <Route path="/contact">contact</Route>
@@ -20,6 +30,9 @@ function App() {
           </Route>
           <Route path="/register">
             <Register />
+          </Route>
+          <Route path="/users">
+            <Users />
           </Route>
           <Route path="/" exact>
             home
