2e9480bd788cbb7ee463f7d99ca7e5b202914392
Hona <nguyenmanhhoan2k4@gmail.com>
2025-08-23 21:41:16 +0700

    completed create modal user

--- src/services/userService.js
+++ src/services/userService.js
@@ -31,4 +31,17 @@ const fetchGroup = () => {
   return axios.get(`http://localhost:8080/api/v1/group/read`);
 };
 
-export { registerNewUser, loginUser, fetchAllUsers, deleteUser, fetchGroup };
+const createNewUser = (userData) => {
+  return axios.post(`http://localhost:8080/api/v1/user/create`, {
+    ...userData,
+  });
+};
+
+export {
+  registerNewUser,
+  loginUser,
+  fetchAllUsers,
+  deleteUser,
+  fetchGroup,
+  createNewUser,
+};
