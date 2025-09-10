d81243bec0451c234299ca7a48015feaf3ad900b
Hona <nguyenmanhhoan2k4@gmail.com>
2025-09-10 21:32:02 +0700

    add loading spinner and customize axios file

--- src/App.js
+++ src/App.js
@@ -3,19 +3,40 @@ import Nav from "./components/Navigation/Nav";
 import { BrowserRouter as Router } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useContext } from "react";
 import AppRoutes from "./routes/AppRoutes";
+import { Rings } from "react-loader-spinner";
+import { UserContext } from "./context/UserContext";
 
 function App() {
- 
+  const { user } = useContext(UserContext);
   return (
-    <Router>
-      <div className="app-header">
-        <Nav />
-      </div>
-      <div className="app-container">
-        <AppRoutes />
-      </div>
+    <>
+      <Router>
+        {user && user.isLoading ? (
+          <div className="loading-container">
+            <Rings
+              height="80"
+              width="80"
+              radius="9"
+              color="#1877f2"
+              ariaLabel="loading"
+              wrapperStyle
+              wrapperClass
+            />
+            <div>Loading data...</div>
+          </div>
+        ) : (
+          <>
+            <div className="app-header">
+              <Nav />
+            </div>
+            <div className="app-container">
+              <AppRoutes />
+            </div>
+          </>
+        )}
+      </Router>
       <ToastContainer
         position="top-right"
         autoClose={5000}
@@ -28,7 +49,7 @@ function App() {
         pauseOnHover
         theme="light"
       />
-    </Router>
+    </>
   );
 }
 export default App;
