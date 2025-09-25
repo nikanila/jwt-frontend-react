4e81fa0c9e03c3f677ca449a8f7c28d97d5651ab
Hona <nguyenmanhhoan2k4@gmail.com>
2025-09-25 21:15:38 +0700

    show list roles and delete role

---
+++ src/components/Role/TableRole.js
@@ -0,0 +1,79 @@
+import { useEffect, useState, forwardRef, useImperativeHandle } from "react";
+import { fetchAllRoles, deleteRole } from "../../services/roleService";
+import { toast } from "react-toastify";
+
+const TableRole = forwardRef((props, ref) => {
+  const [listRoles, setListRoles] = useState([]);
+
+  useEffect(() => {
+    getAllRoles();
+  }, []);
+
+  useImperativeHandle(ref, () => ({
+    fetchListRoleAgain() {
+      getAllRoles();
+    },
+  }));
+
+  const getAllRoles = async () => {
+    let data = await fetchAllRoles();
+    if (data && data.EC === 0) {
+      setListRoles(data.DT);
+    }
+  };
+
+  const handleDeleteRole = async (role) => {
+    let data = await deleteRole(role);
+    if (data && data.EC === 0) {
+      toast.success(data.EM);
+      await getAllRoles();
+    }
+  };
+  return (
+    <table className="table table-bordered table-hover">
+      <thead>
+        <tr>
+          <th scope="col">ID</th>
+          <th scope="col">URL</th>
+          <th scope="col">Description</th>
+          <th>Actions</th>
+        </tr>
+      </thead>
+      <tbody>
+        {listRoles && listRoles.length > 0 ? (
+          <>
+            {listRoles.map((item, index) => {
+              return (
+                <tr key={`row-${index}`}>
+                  <td>{item.id}</td>
+                  <td>{item.url}</td>
+                  <td>{item.description}</td>
+
+                  <td>
+                    <span
+                      title="Delete"
+                      className="delete"
+                      onClick={() => handleDeleteRole(item)}
+                    >
+                      <i className="fa fa-trash-o"></i>
+                    </span>
+                  </td>
+                </tr>
+              );
+            })}
+          </>
+        ) : (
+          <>
+            <tr>
+              <td colSpan={4} align="center">
+                Not found roles
+              </td>
+            </tr>
+          </>
+        )}
+      </tbody>
+    </table>
+  );
+});
+
+export default TableRole;
